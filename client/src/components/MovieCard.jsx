27d504eafaa6bad82c038bb46d8afcd3584b0268
shwetsingh1409 <shwetsingh1409@gmail.com>
2025-08-01 00:14:32 +0530

    add some movies in the home pages

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -7,29 +7,28 @@ const MovieCard = ({ movie }) => {
 
   return (
     <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-2xl hover:-translate-y-1 transition duration-300 w-64'>
+
       <img
         onClick={() => {
           navigate(`/movies/${movie._id}`)
-          window.scrollTo(0, 0)
+          scrollTo(0, 0)
         }}
-        src={movie.backdrop?.path}
-        alt=''
+        src={movie.backdrop_path}
+        alt={movie.title}
         className='rounded-lg h-52 w-full object-cover object-right-bottom cursor-pointer'
       />
 
       <p className='font-semibold mt-2 truncate'>{movie.title}</p>
 
       <p className='text-sm text-gray-400 mt-2'>
-        {new Date(movie.release_date).getFullYear()} •{' '}
-        {movie.genres?.slice(0, 2).map((genre) => genre.name).join(' | ')} •{' '}
-        {movie.runtime}
+        {new Date(movie.release_date).getFullYear()} • {movie.genres?.slice(0, 2).map(genre => genre.name).join(' | ')} • {movie.runtime}
       </p>
 
       <div className='flex items-center justify-between mt-4 pb-3'>
         <button
           onClick={() => {
             navigate(`/movies/${movie._id}`)
-            window.scrollTo(0, 0)
+            scrollTo(0, 0)
           }}
           className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'
         >
