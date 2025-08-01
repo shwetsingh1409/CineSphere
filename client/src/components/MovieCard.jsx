7d0e095db32ba4673ea69b297b4342a930e2b566
shwetsingh1409 <shwetsingh1409@gmail.com>
2025-08-01 23:25:34 +0530

    Add Trailer section on the home page

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 import { StarIcon } from 'lucide-react'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import timeFormat from '../lib/timeFormat'
 
 const MovieCard = ({ movie }) => {
   const navigate = useNavigate()
@@ -21,7 +22,7 @@ const MovieCard = ({ movie }) => {
       <p className='font-semibold mt-2 truncate'>{movie.title}</p>
 
       <p className='text-sm text-gray-400 mt-2'>
-        {new Date(movie.release_date).getFullYear()} • {movie.genres?.slice(0, 2).map(genre => genre.name).join(' | ')} • {movie.runtime}
+        {new Date(movie.release_date).getFullYear()} • {movie.genres?.slice(0, 2).map(genre => genre.name).join(' | ')} • {timeFormat(movie.runtime)}
       </p>
 
       <div className='flex items-center justify-between mt-4 pb-3'>
